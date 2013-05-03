a32133fc960c8f17a5db4b960bb9040fe505e7bd
Rob Martin <rob@version2beta.com>
2013-05-03 18:01:41 -0500

    Don't repeat yourself.

--- sources.py
+++ sources.py
@@ -10,11 +10,11 @@ for k in C.keys():
   if not os.path.exists(mydir + k):
     print "mkdir %s%s" % (mydir, k)
     os.makedirs(mydir + k)
-  for source in C[k]:
-    file_name = mydir + k + source.split('/')[-1]
-    print "wget -nc -O %s %s" % (file_name, source)
-    wget('-nc', '-O', file_name, source)
-    print "unzip -d %s %s" % (mydir + k, file_name)
-    unzip('-d', mydir + k, file_name)
-    print "rm %s" % file_name
-    rm(file_name)
+    for source in C[k]:
+      file_name = mydir + k + source.split('/')[-1]
+      print "wget -nc -O %s %s" % (file_name, source)
+      wget('-nc', '-O', file_name, source)
+      print "unzip -d %s %s" % (mydir + k, file_name)
+      unzip('-d', mydir + k, file_name)
+      print "rm %s" % file_name
+      rm(file_name)
